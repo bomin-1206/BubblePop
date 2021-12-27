34983e1919cbae13a0ddca6d3c685d9a272fb829
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2021-12-27 14:33:52 +0900

    Update .sql

--- .sql
+++ .sql
@@ -1,7 +1,7 @@
-create database date;
-use date;
+create database 'date';
+use 'date';
 /*회원가입/로그인 db*/
-create table users(
-          id varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL PRIMARY KEY,
-          password varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
-      )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+create table 'users'(
+    id varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL PRIMARY KEY,
+    password varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
