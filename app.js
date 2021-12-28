50708caf2e27aeac94a8b1f7cd6bed0974faff9c
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2021-12-28 11:04:28 +0900

    server

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var logoutRouter = require('./routes/logout')
 
 var app=express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
