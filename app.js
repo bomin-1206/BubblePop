572f4e029d9352ce7d8190aba4c83adfaa368ce0
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2021-12-28 17:58:22 +0900

    server

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session=require('express-session');
 
-const mainRouter = require('./routes/main');
+//const mainRouter = require('./routes/main');
 const indexRouter = require('./routes/index');
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
@@ -23,30 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-    secret:'hackathon',//보안
-    resave:false,
-    saveUninitialized:true//익명
-}));
-
-
 app.use('/', indexRouter);
-app.use('/main', mainRouter);
+//app.use('/main', mainRouter);
+app.use('/register', registerRouter);
 app.use('/login', loginRouter);
 app.use('/logout',logoutRouter);
-app.use('/register', registerRouter);
-
-app.use(function(req, res, next) {
-    next(createError(404));
-  });
-
-app.use(function(err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
   
 module.exports = app;
--- database.sql
+++ database.sql
@@ -1,11 +1,26 @@
-create database 'date';
-use 'date';
+create database date;
+use date;
 /*회원가입/로그인 db*/
-create table 'users'(
+create table users(
     id varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL PRIMARY KEY,
     password varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-create table ''(
-    
-);
+/*카페,레스토랑,장소 위도,경도,이름 db*/
+create table CAFE(
+    name varchar(20) NOT NULL PRIMARY KEY,
+    x_location decimal(16,12) NOT NULL,
+    y_location decimal(16,12) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+create table RESTAURANT(
+    name varchar(20) NOT NULL PRIMARY KEY,
+    x_location decimal(16,12) NOT NULL,
+    y_location decimal(16,12) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+create table PLACE(
+    name varchar(20) NOT NULL PRIMARY KEY,
+    x_location decimal(16,12) NOT NULL,
+    y_location decimal(16,12) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
