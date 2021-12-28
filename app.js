396e7c909ebc170b94c59cf691e5bf4c40f0c0b3
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2021-12-28 12:33:12 +0900

    .

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
     secret:'hackathon',//보안
