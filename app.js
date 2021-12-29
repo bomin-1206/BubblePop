0049611d79282b6f3f86882dfd13e9e17a4a7da3
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2021-12-29 15:36:47 +0900

    회원가입

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const session=require('express-session');
+const bodyParser = require('body-parser');
+var mysql = require('mysql');
 
-const mainRouter = require('./routes/main');
-const registerRouter = require('./routes/register');
-const loginRouter = require('./routes/login');
-const logoutRouter = require('./routes/logout')
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1111',
+    database: 'date',
+});
+
+conn.connect();
 
 const app=express();
 
@@ -21,6 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/public/views/login.html");
 });
@@ -30,9 +38,44 @@ app.get("/register.html", function (req, res) {
 app.get("/login.html", function (req, res) {
     res.sendFile(__dirname + "/public/views/login.html");
 });
+app.get("/home.html", function (req, res) {
+    res.sendFile(__dirname + "/public/views/home.html");
+});
 
-app.use('/main', mainRouter);
-app.use('/register', registerRouter);
-app.use('/login', loginRouter);
-app.use('/logout',logoutRouter);
+app.post("/register",function(req,res){
+    const param =[req.body.id,req.body.pswd1];
+    conn.query("INSERT INTO users(id,password) VALUES(?,?)",param,function(err,result,fields){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        console.log('inserted');
+    })
+});
+app.post('/login', function(req, res){
+    const userId= req.body.id;
+    const password=req.body.pw;
+    let result='';
+  
+    const rows = conn.query('select id, password from users where id=?',[userId])
+    conn.end();
+  
+    if(rows.length==0){
+      result='등록되지 않은 사용자 입니다.';
+    }
+    else {
+      const db_user=rows[0];
+      if(db_user.user_password==password){
+        result='반갑습니다.';
+        req.session.userId=userId;
+        res.redirect('/main');
+        return;
+      }
+      else
+        result='아이디 또는 비밀번호가 잘못 입력되었습니다.';
+    }
+   
+    res.render('login', {result:result});
+  });
+
 module.exports = app;
